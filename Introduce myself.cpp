a3826ccc45c222d5d12d81f7b386241c863d897c
Annisa Citra Pratiwi <112589538+AnnisaCitraPratiwi@users.noreply.github.com>
2023-02-20 16:45:22 +0700

    Add files via upload

---
+++ Introduce myself.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main (){
+ string a;
+ cout<<"Nama Lengkap : ";
+ getline (cin,a);
+ 
+ string b;
+ cout<<"Kelas : ";
+ getline(cin,b);
+ 
+ string c;
+ cout<<"Input Nilai DDP : ";
+ getline (cin,c);  
+ 
+ 	cout<<"--------------------------------"<<endl;
+    cout<<a<<endl<<b<<endl<<c;
+    
+ return 0;
+}
